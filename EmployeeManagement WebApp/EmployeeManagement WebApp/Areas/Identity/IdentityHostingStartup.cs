f28e6bb9bff69efee78f7678885f4b1e960f95ce
Rikesh Bhakta <rikesh.b@simformsolutions.com>
2023-01-12 11:52:58 +0530

    intial commit

---
+++ EmployeeManagement WebApp/EmployeeManagement WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using EmployeeManagement_WebApp.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(EmployeeManagement_WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace EmployeeManagement_WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<EmployeeManagement_WebAppContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("EmployeeManagement_WebAppContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<EmployeeManagement_WebAppContext>();
+            });
+        }
+    }
+}
---
+++ EmployeeManagement WebApp/EmployeeManagement WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeManagement_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
